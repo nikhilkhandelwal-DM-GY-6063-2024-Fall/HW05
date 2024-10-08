4fd4383e8a0ff7fa7b8e49c9b7de8a6a5ce4b0eb
nikhil0119 <nikhilkhandelwal0119@gmail.com>
2024-10-07 23:35:15 -0400

    Part A code

--- 0/sketch.js
+++ 0/sketch.js
@@ -1,12 +1,48 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);  
 }
 
 function draw() {
-  background(255);
-  fill(0);
-  rect(width / 2.5, height / 4, width / 20, height / 2);
-  rect(width - width / 2.5 - width / 20, height / 4, width / 20, height / 2);
-  rect(width / 2.5 + width / 20, height / 4 - width / 20, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
-  rect(width / 2.5 + width / 20, height / 4 + height / 2, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
+  background(0); 
+  
+  translate(width/2, height/2);
+  rotate(-90);
+  
+  let hr=hour();
+  let mn=minute();
+  let sc=second();
+  
+  strokeWeight(8);
+  stroke(255);
+  noFill();
+  ellipse(0,0,400,400);
+  
+  let secondAngle=map(sc,0,60,0,360);
+  stroke(255, 0, 0);
+  strokeWeight(4);
+  line(0,0,150*cos(secondAngle),150*sin(secondAngle));
+  
+  let minuteAngle = map(mn,0,60,0,360);
+  stroke(255);
+  strokeWeight(6);
+  line(0, 0, 120 * cos(minuteAngle), 120 * sin(minuteAngle));
+  
+  let hourAngle = map(hr%12,0,12,0,360);
+  stroke(100,255,100);
+  strokeWeight(8);
+  line(0,0,90*cos(hourAngle),90*sin(hourAngle));
+  
+  stroke(255);
+  point(0,0);
+  
+  strokeWeight(2);
+  for (let i=0; i<360; i+=30) {
+    let x1=180*cos(i);
+    let y1=180*sin(i);
+    let x2=160*cos(i);
+    let y2=160*sin(i);
+    stroke(255);
+    line(x1,y1,x2,y2);
+  }
 }
